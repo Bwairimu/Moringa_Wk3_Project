93d7556a15f3b10d5a11de8bd5dff3bcad66a53b
Bah Wairimu <bah.irimu@gmail.com>
2022-01-04 10:22:01 +0300

    Added form validation

---
+++ js/script.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict'
+    var Forms = document.querySelectorAll('.needs-validation')
+    // Loop over them and prevent submission
+    Array.prototype.slice.callform)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
