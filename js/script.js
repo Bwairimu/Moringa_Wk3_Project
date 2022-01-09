83115d5c7e34d6d0c60d84037a7a71ad3645d6f8
Bah Wairimu <bah.irimu@gmail.com>
2022-01-09 23:03:45 +0300

    Added changes to the home-page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+const navSlide= () => {
+  const menu=document.querySelector('.menu');
+  const nav=document.querySelector('.nav-link');
+  const navLinks=document.querySelectorAll('.nav-link li');
+
+  menu.addEventListener('click',() => {
+      //Toggle nav
+    nav.classList.toggle( '.nav-active');
+    //animate
+    navLinks.forEach(link,index) => {
+      if(link.style.animation) {
+        link.style.animation='';
+      } else {
+        link.style.animation='navLinkFade 0.5s ease forwards';
+      }
+    });
+  });
+}
+navSlide();
+
+
 (function () {
     'use strict'
     var Forms = document.querySelectorAll('.needs-validation')
