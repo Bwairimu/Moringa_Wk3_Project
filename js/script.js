730bf8fe3e06b9f6b0e08478abd1f3e76146311e
Bah Wairimu <bah.irimu@gmail.com>
2022-01-29 06:04:29 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,41 +1,35 @@
 $(document).ready(function () {
-  $("#img1").click(function () {
-   $(this).hide();
-   $("#show_hide").show();
-  });
+var target=".hidden";
+var slide_speed=3000;
+$(target).hide();
+$(target).slideDown(slide_speed);
+$("#dImg").on("click",function() {
+  $(target).slideUp();
 });
-$(document).ready(function () {
-  $("#show_hide").click(function () {
-    $(this).hide();
-    $("#img1").show();
-  });
 });
+
 $(document).ready(function () {
-  $("#img2").click(function () {
-    $(this).hide();
-    $("#show_hide1").show();
-  });
+var target=".hidden";
+var slide_speed=3000;
+$(target).hide();
+$(target).slideDown(slide_speed);
+$("#devImg").on("click",function() {
+  $(target).slideUp();
 });
-$(document).ready(function () {
-  $("#show_hide1").click(function () {
-    $(this).hide();
-    $("#img2").show();
-  });
 });
+
 $(document).ready(function () {
-  $("#img3").click(function () {
-    $(this).hide();
-    $("#show_hide2").show();
-  });
+var target=".hidden";
+var slide_speed=3000;
+$(target).hide();
+$(target).slideDown(slide_speed);
+$("#prodImg").on("click",function() {
+  $(target).slideUp();
 });
-$(document).ready(function () {
-  $("#show_hide2").click(function () {
-    $(this).hide();
-    $("#img3").show();
-  });
 });
+
 $(document).ready(function () {
-  $("#work1,#work2,#work3,#work4,#work5,#work6,#work7,#work8").mouseenter(function () {
+  $("#work1,#work2,#work3,#work4,#work5,#work6,#work7,#work8").mouseover(function () {
     alert("Portfolio!");
   });
 });
